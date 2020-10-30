d6ff2d06bbf9facc38c3c0d24d4fa6bd6d245d75
kundankabra <kundan.21810729@viit.ac.in>
2020-10-30 22:28:12 +0530

    Updates

---
+++ precache-manifest.ea82cbe82d7df22a0d7e7a125cbc9985.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43cd81980159225da36ddb17f4d01906",
+    "url": "/upcloud-mbs-application-form/index.html"
+  },
+  {
+    "revision": "1da22e9fe2d78d7ffd3a",
+    "url": "/upcloud-mbs-application-form/static/css/2.bb4ba17c.chunk.css"
+  },
+  {
+    "revision": "e5ca17207253d13e374e",
+    "url": "/upcloud-mbs-application-form/static/css/main.e68d570e.chunk.css"
+  },
+  {
+    "revision": "1da22e9fe2d78d7ffd3a",
+    "url": "/upcloud-mbs-application-form/static/js/2.3429aa2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/upcloud-mbs-application-form/static/js/2.3429aa2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5ca17207253d13e374e",
+    "url": "/upcloud-mbs-application-form/static/js/main.6ed99c09.chunk.js"
+  },
+  {
+    "revision": "95e0ef07fb2b681aaf39",
+    "url": "/upcloud-mbs-application-form/static/js/runtime-main.23462c58.js"
+  }
+]);
